3b616b96b477961fed951863cb815733796797ae
Eva Montejano <evamontejano30@gmail.com>
2023-12-06 12:02:10 +0200

    delete monitor

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,6 @@ const Hero = () => {
         </div>
       </div>
 
-        <ComputersCanvas/>
 
         <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
           <a href="#about">
